abe143ae15495c2be9b0e4c8dc74154e10be05a8
Shivam Ujjainwal <shivam.jobs056@gmail.com>
2025-03-05 11:56:21 -0500

    added routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { TaskManagerComponent } from './features/task-manager/task-manager.component';
+
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskManagerComponent }
+];
---
+++ src/app/features/task-manager/task-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+@Component({
+  imports: [AddTaskComponent, TaskListComponent, CommonModule],
+  selector: 'app-task-manager',
+  templateUrl: './task-manager.component.html',
+  styleUrls: ['./task-manager.component.scss']
+})
+export class TaskManagerComponent { }
